9209480d6d4a22020818f6169f8da4a60bd77a4e
Nikhil Tanneeru <121566002+NikhilTanneeru@users.noreply.github.com>
2024-05-25 14:19:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
   return (
     <Router>
         <Navbar/>
-        <div className="container my-5">
-            <Home/>
-        </div> 
+        <Home/>
         <Contact/>
     </Router>
         
