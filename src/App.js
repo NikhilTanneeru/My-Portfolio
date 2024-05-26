3156429bc73ca649a94f9e42f1fefa421ddec016
Nikhil Tanneeru <121566002+NikhilTanneeru@users.noreply.github.com>
2024-05-26 12:09:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <>
         <Navbar/>
         <Home/>
         <Contact/>
-    </Router>
+    <>
         
     );
 }
