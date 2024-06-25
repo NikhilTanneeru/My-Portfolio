4e2e657801a0107e2a3cbb57be5e355754a0d410
Nikhil Tanneeru <121566002+NikhilTanneeru@users.noreply.github.com>
2024-06-25 17:13:51 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,10 +7,34 @@ export default function Projects(props) {
     <div className="container certbox-m">
       <center><h3 className="heading py-3">Projects</h3></center>
       <div className="container cont">
-        <ProjectCard title={'PayRoll Management System'} imgName='PayRoll.png' desc='Java, JavaFx, Apache Poi, Spreadsheet as Database, Scene Builder' role='Team Lead'/>
-        <ProjectCard title={'Infix to Postfix Converter'} imgName='InfixPostfix.png' desc='HTML, CSS, JavaScript with dynamic programming' role='Team Lead'/>
-        <ProjectCard title={'Random Password Generator'} imgName='PayRoll.png' desc='Python, Libraries such as Random' role='Team Lead'/>
-        <ProjectCard title={'Online Apraisal'} imgName='PayRoll.png' desc='JavaScript, React, Node, CSS, Bootstrap, MongoDB' role='Team Lead'/>
+        <ProjectCard 
+            title={'PayRoll Management System'} 
+            imgName='PayRoll.png' 
+            desc='Java, JavaFx, Apache Poi, Spreadsheet as Database, Scene Builder' 
+            role='Team Lead'
+            githubLink="https://github.com/NikhilTanneeru/PayRoll"  
+          />
+        <ProjectCard 
+              title={'Infix to Postfix Converter'} 
+              imgName='InfixPostfix.png' 
+              desc='HTML, CSS, JavaScript with dynamic programming' 
+              role='Team Lead'
+              githubLink="https://github.com/NikhilTanneeru/Expression-Converter"
+        />
+        <ProjectCard 
+                title={'Random Password Generator'} 
+                imgName='PayRoll.png' 
+                desc='Python, Libraries such as Random' 
+                role='Team Lead'
+                githubLink="https://github.com/NikhilTanneeru/Random-Password-Generator"
+        />
+        <ProjectCard 
+                title={'Online Apraisal'} 
+                imgName='PayRoll.png' 
+                desc='JavaScript, React, Node, CSS, Bootstrap, MongoDB' 
+                role='Team Lead'
+                
+          />
       </div>
     </div>
   );
