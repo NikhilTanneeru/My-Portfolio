0b43cda8ccc48663c629fe1992751aa29bbbb1e1
Nikhil Tanneeru <121566002+NikhilTanneeru@users.noreply.github.com>
2024-06-25 17:04:56 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,6 +2,10 @@ import React from 'react'
 import '../css/Certificate.css'
 
 export default function ProjectCard(props) {
+  const openGithubLink = () => {
+    window.open(props.githubLink, '_blank');
+  }
+
   return (
     <div className='container cert-view'>
         <img className='cert-img' src={props.imgName} alt="image1" />
@@ -9,7 +13,7 @@ export default function ProjectCard(props) {
             <center><h5 className="cert-title py-3 " style={{marginTop:'3px'}}>{props.title}</h5></center>
             <p className='px-3' style={{paddingTop:'3px'}}><strong>Technologies Used: </strong>{props.desc}</p>
             <p className='px-3' style={{paddingTop:'3px'}}><strong>Role: </strong>{props.role}</p>
-            <center><button className='btn btn-primary'>Open Credential</button></center>
+            <center><button className='btn btn-primary' onClick={openGithubLink}>Open Credential</button></center>
         </div>
     </div>
   )
